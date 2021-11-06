9b3e57e977ab16202f43c03080437b003ba50e66
System Administrator <root@Thomass-iMac.fritz.box>
2021-11-06 22:16:55 +0100

    trying to deploy to heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+const express = require('express');
+const app = express();
+app.use(express.static('./dist/MyArticlesBlog'));
+app.get('/*', function(req, res) {
+    res.sendFile('src/index.html', {root: 'dist/MyArticlesBlog/'}
+  );
+});
+app.listen(process.env.PORT || 8080);
