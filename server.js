2491b2eddbe77bb9bfe6c0ffefc865e9fd77966b
System Administrator <root@Thomass-iMac.fritz.box>
2021-11-23 18:52:27 +0100

    trying to fix 404

--- server.js
+++ server.js
@@ -7,23 +7,12 @@ function requireHTTPS(req, res, next) {
 }
 const express = require('express');
 const app = express();
-//app.use(express.static('../dist/MyArticlesBlog/'));
-app.use(express.static('./'));
-app.get('/', function(req, res) {
+app.use(express.static('./dist/MyArticlesBlog/'));
+//app.use(express.static('./'));
+app.get('/*', function(req, res) {
     res.sendFile('www/index.html',
-    {root:__dirname});//, {root: 'dist/MyArticlesBlog/'}
-    res.sendFile('www/main-es2015.js', 
-    {root:__dirname});
-
-    res.sendFile('www/polyfills-es2015.js', 
-    {root:__dirname});
-
-    res.sendFile('www/vendor-es2015.js', 
-    {root:__dirname});
-
-    res.sendFile('www/runtime-es2015.js', 
-    {root:__dirname});
-
+    {root:'dist/MyArticlesBlog/'});//, {root: 'dist/MyArticlesBlog/'}
+    
 
   //);
 });
